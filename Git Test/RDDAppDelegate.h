0327cf0e01bed94061abd5c27d73a77482a4450f
Ron Duran <ronduran@Ron-Durans-MacBook-Pro.local>
2014-09-07 15:34:05 -0700

    Initial Commit

---
+++ Git Test/RDDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RDDAppDelegate.h
+//  Git Test
+//
+//  Created by Ron Duran on 9/7/14.
+//  Copyright (c) 2014 RDuran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git Test/RDDViewController.h
@@ -0,0 +1,13 @@
+//
+//  RDDViewController.h
+//  Git Test
+//
+//  Created by Ron Duran on 9/7/14.
+//  Copyright (c) 2014 RDuran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RDDViewController : UIViewController
+
+@end
