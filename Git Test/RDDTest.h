c87db7174c07cf66553dbd7346a0a930d5d97390
Ron Duran <ronduran@Ron-Durans-MacBook-Pro.local>
2014-09-07 15:59:03 -0700

    Added Test class

---
+++ Git Test/RDDTest.h
@@ -0,0 +1,13 @@
+//
+//  RDDTest.h
+//  Git Test
+//
+//  Created by Ron Duran on 9/7/14.
+//  Copyright (c) 2014 RDuran. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RDDTest : NSObject
+
+@end
---
+++ Git Test/RDDTest.m
@@ -0,0 +1,13 @@
+//
+//  RDDTest.m
+//  Git Test
+//
+//  Created by Ron Duran on 9/7/14.
+//  Copyright (c) 2014 RDuran. All rights reserved.
+//
+
+#import "RDDTest.h"
+
+@implementation RDDTest
+
+@end
